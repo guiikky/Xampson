6dd90f1fefcb0119ca18a84e90ea27f8e6ae9d9e
guiikky <guilherme_rib.silva@hotmail.com>
2016-03-29 12:35:24 -0300

    pao

---
+++ CaixaEletronico/src/negocio/Dispenser.java
@@ -0,0 +1,112 @@
+package negocio;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Formatter;
+
+import idioma.Idioma;
+
+public class Dispenser {
+	private int vet[];
+
+	// Metodo contrutor apartir da quantidade de notas no txt
+	public Dispenser() {
+		File arquivo = new File(System.getProperty("user.dir") + "\\src\\negocio\\dispenser.txt");
+		vet = new int[3];
+		int i = 0;
+		try (FileReader fr = new FileReader(arquivo); BufferedReader br = new BufferedReader(fr);) {
+			while (br.ready()) {
+				vet[i++] = Integer.parseInt(br.readLine());
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	// Metodos auxiliares
+	private boolean n10() {
+		return vet[0] == 0;
+	}
+
+	private boolean n20() {
+		return vet[1] == 0;
+	}
+
+	private boolean n50() {
+		return vet[2] == 0;
+	}
+
+	// Metodo que mostra quais notas estao disponiveis
+	public String status() {
+		if (!n10() && !n20() && !n50()) {
+			return Idioma.getRb().getString("dispenser1");
+		} else if (!n10() && !n20() && n50()) {
+			return Idioma.getRb().getString("dispenser2");
+		} else if (!n10() && n20() && !n50()) {
+			return Idioma.getRb().getString("dispenser3");
+		} else if (n10() && !n20() && !n50()) {
+			return Idioma.getRb().getString("dispenser4");
+		} else if (!n10() && n20() && n50()) {
+			return Idioma.getRb().getString("dispenser5");
+		} else if (n10() && !n20() && n50()) {
+			return Idioma.getRb().getString("dispenser6");
+		} else if (n10() && n20() && !n50()) {
+			return Idioma.getRb().getString("dispenser7");
+		} else {
+			return Idioma.getRb().getString("dispenser8");
+		}
+	}
+
+	public String extratoNotas() {
+		return "Notas de 10 = " + vet[0] + "\nNotas de 20 = " + vet[1] + "\nNotas de 50 = " + vet[2];
+	}
+
+	// Metodo que faz a melhor contagem de notas (Resquisito)
+	public boolean contar(int x) {
+		int y = x;
+		int a, b, c;
+		a = b = c = 0;
+		while (x - 50 >= 0 && vet[0] - c > 0) {
+			x -= 50;
+			c++;
+		}
+
+		while (x - 20 >= 0 && vet[1] - b > 0) {
+			x -= 20;
+			b++;
+		}
+
+		while (x - 10 >= 0 && vet[2] - a > 0) {
+			x -= 10;
+			a++;
+		}
+		if (x == 0) {
+			vet[0] -= a;
+			vet[1] -= b;
+			vet[2] -= c;
+			System.out.println("R$" + y + ",00\n");
+			System.out.println("R$50,00   R$20,00   R$10,00");
+			System.out.println(n(a + "") + a + "x   " + n(b + "") + b + "x   " + n(c + "") + c + "x\n");
+			try (Formatter saida = new Formatter(System.getProperty("user.dir") + "\\src\\negocio\\dispenser.txt");) {
+				String quebraDeLinha = System.getProperty("line.separator");
+				for (int i = 0; i < vet.length; i++) {
+					saida.format(vet[i] + quebraDeLinha);
+				}
+			} catch (Exception e) {
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private static String n(String x) {
+		String resp = "";
+		int y = 6 - x.length();
+		for (int i = 0; i < y; i++) {
+			resp += " ";
+		}
+		return resp;
+	}
+
+}
---
+++ CaixaEletronico/src/to/ContaTO.java
@@ -0,0 +1,66 @@
+package to;
+
+import negocio.Cliente;
+
+public class ContaTO {
+	private int conta;
+	private int agencia;
+	private double saldo;
+	private Cliente cliente;
+
+	public int getConta() {
+		return conta;
+	}
+
+	public void setConta(int conta) {
+		this.conta = conta;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContaTO other = (ContaTO) obj;
+		if (agencia != other.agencia)
+			return false;
+		if (cliente == null) {
+			if (other.cliente != null)
+				return false;
+		} else if (!cliente.equals(other.cliente))
+			return false;
+		if (conta != other.conta)
+			return false;
+		if (Double.doubleToLongBits(saldo) != Double.doubleToLongBits(other.saldo))
+			return false;
+		return true;
+	}
+
+}
